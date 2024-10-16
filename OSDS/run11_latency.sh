2280d1880b2fdbe5f2f91eca191f864b65d18a03
任毅 <yi-ren20@mails.tsinghua.edu.cn>
2024-10-16 14:47:26 +0000

    Delete Useless Files

--- OSDS/run11_latency.sh
+++
@@ -1,4 +0,0 @@
-echo "started"
-go run sync.go LATENCYTEST 300 300
-go run sync.go LATENCYTEST 300 30
-go run sync.go LATENCYTEST 30 30
--- OSDS/testserver.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	sum := 0
-	for i := 1; i < 1000000000; i++ {
-		sum += i * i * (i + 1)
-	}
-}
